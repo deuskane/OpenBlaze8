37e68f540e4f89a2c76726a9e094cc8a24b82398
deuskane <deuskane@gmail.com>
2017-04-10 13:02:28 +0200

    Add Makefile and create infra directory

---
+++ environment.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+function environment_main ()
+{
+    local path=$(dirname $(readlink -f $BASH_SOURCE));
+
+    export BOARD="Basys1";
+
+    export ASYLUM_HOME="${path}";
+    export ASYLUM_RTL_HOME="${ASYLUM_HOME}/rtl";
+    export ASYLUM_INFRA_HOME="${ASYLUM_HOME}/infra";
+
+    echo "BOARD              : ${BOARD}";
+    echo "ASYLUM_HOME        : ${ASYLUM_HOME}";
+    echo "ASYLUM_RTL_HOME    : ${ASYLUM_RTL_HOME}";
+    echo "ASYLUM_INFRA_HOME  : ${ASYLUM_INFRA_HOME}";
+}
+
+environment_main $*;
---
+++ infra/mk/ip.mk
@@ -0,0 +1,26 @@
+ifeq ("$(ASYLUM_RTL_HOME)","")
+$(error Environment variable "ASYLUM_RTL_HOME" must be set)
+endif
+
+PATH_BUILD	= $(CURDIR)/build
+PATH_EXTRACT	?= $(PATH_BUILD)/extract
+
+export PATH_EXTRACT
+
+NAME		= $(notdir $(CURDIR))
+
+FILES_VHDL	+= $(wildcard src/*.vhd)
+
+extract		: $(PATH_EXTRACT)
+		$(foreach deps,$(IP_DEPS),$(MAKE) $(MFLAGS) -C $(ASYLUM_RTL_HOME)/$(deps) extract;)
+		cp $(FILES_VHDL) $(PATH_EXTRACT)
+
+clean		:
+		rm -fr $(PATH_BUILD)
+
+
+$(PATH_BUILD) $(PATH_EXTRACT) :
+		mkdir -p $@
+
+.PHONY		: 	extract	\
+			clean
