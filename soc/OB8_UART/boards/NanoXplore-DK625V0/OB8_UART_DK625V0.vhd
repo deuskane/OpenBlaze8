6c6edd2cf52e352849981cabd0e36d3746ee3809
mrosiere <derekkarbarer@gmail.com>
2017-04-11 13:00:13 +0200

    Add new board : NanoXplore-DK625V0

---
+++ soc/OB8_UART/boards/NanoXplore-DK625V0/OB8_UART_DK625V0.vhd
@@ -0,0 +1,65 @@
+-------------------------------------------------------------------------------
+-- Title      : OB8_UART_DK625V0
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : OB8_UART_DK625V0.vhd
+-- Author     : Cédric DEBARGE
+-- Company    :
+-- Created    : 2017-03-31
+-- Last update: 2017-04-11
+-- Platform   :
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2017 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2017-03-31  1.0      cdebarge Created
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library work;
+
+entity OB8_UART_DK625V0 is
+  port (
+    clk_i    : in  std_logic;
+    arstn_i  : in  std_logic;
+
+    -- GPIOs
+    switch_i : in  std_logic_vector(5 downto 0);
+    led_o    : out std_logic_vector(7 downto 0);
+
+    --UART
+    srx_i    : in std_logic;
+    stx_o    : out std_logic;
+    bdr_o    : out std_logic
+    );
+end OB8_UART_DK625V0;
+
+architecture rtl of OB8_UART_DK625V0 is
+  constant FSYS       : positive := 50_000_000;
+  constant FSYS_INT   : positive := 50_000_000;
+
+begin  -- architecture rtl
+  ins_OB8_UART : entity work.OB8_UART(rtl)
+  generic map
+    (FSYS      => FSYS    
+    ,FSYS_INT  => FSYS_INT
+    ,NB_SWITCH => 6
+    ,NB_LED    => 8
+    )
+  port
+    (clk_i    => clk_i   
+    ,arstn_i  => arstn_i 
+    ,switch_i => switch_i
+    ,led_o    => led_o   
+    ,srx_i    => srx_i   
+    ,stx_o    => stx_o   
+    ,bdr_o    => bdr_o   
+    );
+
+end architecture rtl;
---
+++ soc/OB8_VGA/boards/NanoXplore-DK625V0/OB8_VGA_DK625V0.vhd
@@ -0,0 +1,77 @@
+-------------------------------------------------------------------------------
+-- Title      : OB8_VGA_DK625V0
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : OB8_VGA_DK625V0.vhd
+-- Author     : Mathieu Rosiere
+-- Company    : 
+-- Created    : 2017-04-11
+-- Last update: 2017-04-11
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2017 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2017-04-11  1.0      mrosiere	Created
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library work;
+
+entity OB8_VGA_DK625V0 is
+  port (
+    clk_i      : in  std_logic;
+    arstn_i    : in  std_logic;
+
+    switch_i   : in  std_logic_vector(5 downto 0);
+    led_o      : out std_logic_vector(7 downto 0);
+
+    vga_HSYNC_o: out std_logic;
+    vga_VSYNC_o: out std_logic;
+    vga_Red_o  : out std_logic;
+    vga_Green_o: out std_logic;
+    vga_Blue_o : out std_logic
+);
+end OB8_VGA_DK625V0;
+
+architecture rtl of OB8_VGA_DK625V0 is 
+  constant FSYS                       : positive:= 50_000_000;
+  constant FSYS_INT                   : positive:= 50_000_000;
+
+  signal vga_Red                      : std_logic_vector (2 downto 0);
+  signal vga_Green                    : std_logic_vector (2 downto 0);
+  signal vga_Blue                     : std_logic_vector (2 downto 1);
+  
+begin  -- architecture rtl
+
+  ins_OB8_VGA : entity work.OB8_VGA(rtl)
+    generic map
+    (FSYS       => FSYS
+    ,FSYS_INT   => FSYS_INT
+    ,NB_SWITCH  => 6
+    ,NB_LED     => 8
+    )
+    port map
+    (clk_i      => clk_i      
+    ,arstn_i    => arstn_i
+    ,switch_i   => switch_i   
+    ,led_o      => led_o      
+    ,vga_HSYNC_o=> vga_HSYNC_o
+    ,vga_VSYNC_o=> vga_VSYNC_o
+    ,vga_Red_o  => vga_Red
+    ,vga_Green_o=> vga_Green
+    ,vga_Blue_o => vga_Blue
+     );
+
+  vga_Red_o   <= vga_Red  (2);
+  vga_Green_o <= vga_Green(2);
+  vga_Blue_o  <= vga_Blue (2);
+end architecture rtl;
+    
+  
