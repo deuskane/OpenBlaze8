26eb54ac8f6dc97a2b2970651b0fdee57b822498
deuskane <deuskane@gmail.com>
2017-04-11 11:32:42 +0200

    Create work dir in build

--- infra/mk/ip.mk
+++ infra/mk/ip.mk
@@ -4,6 +4,7 @@ endif
 
 PATH_BUILD	= $(CURDIR)/build
 PATH_EXTRACT	?= $(PATH_BUILD)/extract
+PATH_WORK	= $(PATH_BUILD)/$(BOARD)
 
 export PATH_EXTRACT
 
@@ -11,15 +12,15 @@ NAME		= $(notdir $(CURDIR))
 
 FILES_VHDL	+= $(wildcard src/*.vhd) $(wildcard boards/$(BOARD)/*)
 
-extract		: $(PATH_EXTRACT)
+extract		: | $(PATH_WORK) $(PATH_EXTRACT)
 		$(foreach deps,$(IP_DEPS),$(MAKE) $(MFLAGS) -C $(ASYLUM_RTL_HOME)/$(deps) extract;)
 		cp $(FILES_VHDL) $(PATH_EXTRACT)
 
 clean		:
-		rm -fr $(PATH_BUILD)
+		rm -fr $(PATH_EXTRACT)
 
 
-$(PATH_BUILD) $(PATH_EXTRACT) :
+$(PATH_BUILD) $(PATH_EXTRACT) $(PATH_WORK) :
 		mkdir -p $@
 
 .PHONY		: 	extract	\
