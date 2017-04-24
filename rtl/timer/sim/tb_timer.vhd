676b8841b30fe2cf21ec460bde8374524cf8d422
deuskane <deuskane@gmail.com>
2017-04-24 09:27:23 +0200

    Add Tick counter, fix bug

---
+++ rtl/timer/sim/tb_timer.vhd
@@ -0,0 +1,260 @@
+-------------------------------------------------------------------------------
+-- Title      : tb_stack
+-- Project    : stack
+-------------------------------------------------------------------------------
+-- File       : tb_stack.vhd
+-- Author     : mrosiere
+-- Company    : 
+-- Created    : 2016-11-11
+-- Last update: 2017-04-26
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2016 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2016-11-11  1.0      mrosiere	Created
+-------------------------------------------------------------------------------
+
+library std;
+use std.textio.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+--use ieee.numeric_bit.all;
+--use ieee.std_logic_arith.all;
+
+library work;
+--use work.timer_pkg.all;
+
+entity tb_timer is
+
+end tb_timer;
+
+architecture tb of tb_timer is
+
+  -- =====[ Constants ]===========================
+  constant FSYS             : positive := 50_000_000;
+  constant TICK_RATIO       : positive :=  5_000_000;
+  constant SIZE_ADDR        : natural  := 3;   
+  constant SIZE_DATA        : natural  := 8;   
+  constant IT_ENABLE        : boolean  := false;
+
+  -- =====[ Signals ]=============================
+  signal clk_i        : std_logic := '0';
+  signal cke_i        : std_logic;
+  signal arstn_i      : std_logic;
+    -- To IP
+  signal cs_i             : std_logic;
+  signal re_i             : std_logic;
+  signal we_i             : std_logic;
+  signal addr_i           : std_logic_vector (SIZE_ADDR-1 downto 0);
+  signal wdata_i          : std_logic_vector (SIZE_DATA-1 downto 0);
+  signal rdata_o          : std_logic_vector (SIZE_DATA-1 downto 0);
+  signal busy_o           : std_logic;
+  signal interrupt_o      : std_logic;
+  signal interrupt_ack_i  : std_logic;
+
+  procedure run
+    (constant n : in positive           -- nb cycle
+    ) is
+    
+    begin
+      for i in 0 to n-1
+      loop
+        wait until rising_edge(clk_i);        
+      end loop;  -- i
+    end run;
+
+begin
+
+  ------------------------------------------------
+  -- Instance of DUT
+  ------------------------------------------------
+  dut : entity work.timer(rtl)
+  generic map
+   (FSYS             => FSYS      
+   ,TICK_RATIO       => TICK_RATIO
+   ,SIZE_ADDR        => SIZE_ADDR 
+   ,SIZE_DATA        => SIZE_DATA 
+   ,IT_ENABLE        => IT_ENABLE 
+    )
+  port map
+   (clk_i            => clk_i          
+   ,cke_i            => cke_i          
+   ,arstn_i          => arstn_i        
+   ,cs_i             => cs_i           
+   ,re_i             => re_i           
+   ,we_i             => we_i           
+   ,addr_i           => addr_i         
+   ,wdata_i          => wdata_i        
+   ,rdata_o          => rdata_o        
+   ,busy_o           => busy_o         
+   ,interrupt_o      => interrupt_o    
+   ,interrupt_ack_i  => interrupt_ack_i
+    );
+
+  
+
+  ------------------------------------------------
+  -- Clock process
+  ------------------------------------------------
+  clk_i <= not clk_i after 10ns;
+  
+  ------------------------------------------------
+  -- Test process
+  ------------------------------------------------
+  -- purpose: Testbench process
+  -- type   : combinational
+  -- inputs : 
+  -- outputs: All dut design with clk_i
+  tb_gen: process is
+  begin  -- process tb_gen
+    report "[TESTBENCH] Test Begin";
+
+    run(1);
+
+    -- Reset
+    report "[TESTBENCH] Reset";
+    arstn_i         <= '0';
+    cke_i           <= '1';
+    cs_i            <= '0';
+    re_i            <= '0';
+    we_i            <= '0';
+    interrupt_ack_i <= '0';
+    run(1);
+    arstn_i         <= '1';
+    run(1);
+    
+    report "[TESTBENCH] ------------------------------------------";
+    report "[TESTBENCH] 1) Set 0xa in counter";
+    report "[TESTBENCH] 2) Start Counter";
+    report "[TESTBENCH] ------------------------------------------";
+    
+    cs_i            <= '1';
+    we_i            <= '1';
+    addr_i          <= "000"; -- control
+    wdata_i         <= X"00"; -- Timer disable
+    run(1);
+
+    addr_i          <= "100"; -- byte 0
+    wdata_i         <= X"0a";
+    run(1);
+
+    addr_i          <= "101"; -- byte 1
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "110"; -- byte 2
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "111"; -- byte 3
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "000"; -- control
+    wdata_i         <= X"01"; -- Timer enable
+    run(1);
+
+    we_i            <= '0';
+    re_i            <= '1';
+
+    run(15);
+
+    cs_i            <= '0';
+    re_i            <= '0';
+
+    
+    report "[TESTBENCH] ------------------------------------------";
+    report "[TESTBENCH] 1) Set 0xa in counter";
+    report "[TESTBENCH] 2) Enable auto start";
+    report "[TESTBENCH] 3) Start Counter";
+    report "[TESTBENCH] ------------------------------------------";
+    
+    cs_i            <= '1';
+    we_i            <= '1';
+
+    addr_i          <= "000"; -- control
+    wdata_i         <= X"00"; -- Timer disable
+    run(1);
+
+    addr_i          <= "100"; -- byte 0
+    wdata_i         <= X"0a";
+    run(1);
+
+    addr_i          <= "101"; -- byte 1
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "110"; -- byte 2
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "111"; -- byte 3
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "000"; -- control
+    wdata_i         <= X"05"; -- Timer enable, auto start enable
+    run(1);
+
+    we_i            <= '0';
+    re_i            <= '1';
+
+    run(100);
+
+    cs_i            <= '0';
+    re_i            <= '0';
+
+    report "[TESTBENCH] ------------------------------------------";
+    report "[TESTBENCH] 1) Set 0xa in counter";
+    report "[TESTBENCH] 2) use tick";
+    report "[TESTBENCH] 3) Start Counter";
+    report "[TESTBENCH] ------------------------------------------";
+    
+    cs_i            <= '1';
+    we_i            <= '1';
+
+    addr_i          <= "000"; -- control
+    wdata_i         <= X"00"; -- Timer disable
+    run(1);
+
+    addr_i          <= "100"; -- byte 0
+    wdata_i         <= X"0a";
+    run(1);
+
+    addr_i          <= "101"; -- byte 1
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "110"; -- byte 2
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "111"; -- byte 3
+    wdata_i         <= X"00";
+    run(1);
+
+    addr_i          <= "000"; -- control
+    wdata_i         <= X"11"; -- Timer enable, tick enable
+    run(1);
+
+    we_i            <= '0';
+    re_i            <= '1';
+
+    run(200);
+
+    cs_i            <= '0';
+    re_i            <= '0';
+
+    report "[TESTBENCH] Test End";
+    wait;
+  end process tb_gen;
+
+  
+end tb;
--- rtl/timer/src/timer.vhd
+++ rtl/timer/src/timer.vhd
@@ -6,7 +6,7 @@
 -- Author     : Mathieu Rosiere
 -- Company    : 
 -- Created    : 2017-04-12
--- Last update: 2017-04-24
+-- Last update: 2017-04-26
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -14,11 +14,11 @@
 -- Register Map :
 -- [0] R/W   : Control
 --             b0 Timer Enable (start/stop)
---             b1 Autostart after Event
---             b2 Event managed by interruption
---             b3 Tick count (else clock count)
--- [1] R/W   : Event
---             b0 Timer Event  (reset)
+--             b1 Timer Event  (reset)
+--             b2 Autostart after Event
+--             b3 Event managed by interruption
+--             b4 Tick count (else clock count)
+-- [1]       : Reserved
 -- [2]       : Reserved
 -- [3]       : Reserved
 -- [4] R/W   : Counter (byte 0)
@@ -47,7 +47,7 @@ entity timer is
     );
 
   port (
-    clk_i : in std_logic;
+    clk_i            : in    std_logic;
     cke_i            : in    std_logic;
     arstn_i          : in    std_logic; -- asynchronous reset
 
@@ -69,12 +69,11 @@ end entity timer;
 
 architecture rtl of timer is
   constant CST0 : std_logic_vector(1024-1 downto 0) := (others => '0');
-  
+  constant TICK : positive := FSYS/TICK_RATIO;
   -----------------------------------------------------------------------------
   -- Address
   -----------------------------------------------------------------------------
   constant raddr_control    : std_logic_vector(SIZE_ADDR-1 downto 0) := std_logic_vector(to_unsigned(0, SIZE_ADDR));
-  constant raddr_event      : std_logic_vector(SIZE_ADDR-1 downto 0) := std_logic_vector(to_unsigned(1, SIZE_ADDR));
   constant raddr_data       : std_logic_vector(SIZE_ADDR-1 downto 0) := std_logic_vector(to_unsigned(4, SIZE_ADDR));
   constant raddr_data_byte0 : std_logic_vector(SIZE_ADDR-1 downto 0) := std_logic_vector(to_unsigned(4, SIZE_ADDR));
   constant raddr_data_byte1 : std_logic_vector(SIZE_ADDR-1 downto 0) := std_logic_vector(to_unsigned(5, SIZE_ADDR));
@@ -82,7 +81,6 @@ architecture rtl of timer is
   constant raddr_data_byte3 : std_logic_vector(SIZE_ADDR-1 downto 0) := std_logic_vector(to_unsigned(7, SIZE_ADDR));
 
   constant waddr_control    : std_logic_vector(SIZE_ADDR-1 downto 0) := raddr_control;
-  constant waddr_event      : std_logic_vector(SIZE_ADDR-1 downto 0) := raddr_event  ;
   constant waddr_data       : std_logic_vector(SIZE_ADDR-1 downto 0) := raddr_data   ;
   constant waddr_data_byte0 : std_logic_vector(SIZE_ADDR-1 downto 0) := raddr_data_byte0;
   constant waddr_data_byte1 : std_logic_vector(SIZE_ADDR-1 downto 0) := raddr_data_byte1;
@@ -99,14 +97,13 @@ architecture rtl of timer is
   signal timer_it_enable_r : std_logic;    -- Timer generate an interruption on event
   signal timer_use_tick_r  : std_logic;    -- Timer source (0 : clock, 1 : tick)
 
-  signal control_r         : std_logic_vector(4 -1 downto 0);
-  signal event_r           : std_logic_vector(1 -1 downto 0);
+  signal control_r         : std_logic_vector(5 -1 downto 0);
 
   signal counter_r         : std_logic_vector(32-1 downto 0);  -- Counter value
-  signal counter_r_we      : std_logic_vector(4 -1 downto 0);  -- Counter write enable
-  signal counter_r_next    : std_logic_vector(32-1 downto 0);  -- Counter value (next)
-
   signal counter_cur_r     : std_logic_vector(32-1 downto 0);  -- Counter value
+
+  signal tick_counter_r    : natural range 0 to TICK-1;
+  
   -----------------------------------------------------------------------------
   -- Signal
   -----------------------------------------------------------------------------
@@ -114,9 +111,19 @@ architecture rtl of timer is
   signal timer_end         : std_logic;    -- Threshold is reach
   signal timer_count       : std_logic;    -- timer counting
 
-  signal counter_r_null    : std_logic;    -- counter_r equal 0
+  signal tick_rst          : std_logic;    -- tick timer reset
+  signal tick_cke          : std_logic;    -- tick timer clock enable
+  signal tick_event        : std_logic;    -- tick timer event
   
---signal counter_read      : std_logic;    -- Threshold is reach
+  signal counter_r_we      : std_logic_vector(4 -1 downto 0);  -- Counter write enable
+  signal counter_r_next    : std_logic_vector(32-1 downto 0);  -- Counter value (next)
+
+  signal counter_cur       : std_logic_vector(32-1 downto 0);
+  signal counter_test      : std_logic_vector(32-1 downto 0);
+  signal counter_cur_r_rst : std_logic;
+  signal counter_cur_r_next: std_logic_vector(32-1 downto 0);
+  signal counter_cur_null  : std_logic;    -- counter_r equal 0
+  
   signal counter_rdata     : std_logic_vector(SIZE_DATA-1 downto 0);
 begin  -- architecture rtl
 
@@ -127,12 +134,6 @@ begin  -- architecture rtl
 
   gen_counter_read_8b: if SIZE_DATA = 8
   generate
---  counter_read  <= '1' when ((addr_i = raddr_data_byte0) or
---                             (addr_i = raddr_data_byte1) or
---                             (addr_i = raddr_data_byte2) or
---                             (addr_i = raddr_data_byte3))
---                   '0';
-    
     counter_rdata <= counter_cur_r( 8-1 downto  0) when (addr_i = raddr_data_byte0) else
                      counter_cur_r(16-1 downto  8) when (addr_i = raddr_data_byte1) else
                      counter_cur_r(24-1 downto 16) when (addr_i = raddr_data_byte2) else
@@ -141,31 +142,24 @@ begin  -- architecture rtl
 
   gen_counter_read_16b: if SIZE_DATA = 16
   generate
---  counter_read  <= '1' when ((addr_i = raddr_data_byte0) or
---                             (addr_i = raddr_data_byte2))
---                   '0';
-    
     counter_rdata <= counter_cur_r(16-1 downto  0) when (addr_i = raddr_data_byte0) else
                      counter_cur_r(32-1 downto 16);
   end generate gen_counter_read_16b;
 
   gen_counter_read_32b: if SIZE_DATA >= 32
   generate
---  counter_read  <= '1' when ((addr_i = raddr_data_byte0))
---                   '0';
-    
     counter_rdata <= std_logic_vector(resize(unsigned(counter_cur_r), counter_rdata'length));
   end generate gen_counter_read_32b;
 
-
+  -- Control register
   control_r <= (timer_use_tick_r  &
-               timer_it_enable_r &
-               timer_autostart_r &
-               timer_enable_r);
+                timer_it_enable_r &
+                timer_autostart_r &
+                timer_event_r     &
+                timer_enable_r);
 
-  event_r(0)<= timer_event_r;
+  -- Read data multiplexor
   rdata_o   <= std_logic_vector(resize(unsigned(control_r), rdata_o'length)) when (addr_i = raddr_control) else
-               std_logic_vector(resize(unsigned(event_r)  , rdata_o'length)) when (addr_i = raddr_event)   else
                counter_rdata;
 
   -----------------------------------------------------------------------------
@@ -173,10 +167,11 @@ begin  -- architecture rtl
   -----------------------------------------------------------------------------
   gen_counter_write_8b: if SIZE_DATA = 8
   generate
-    counter_r_we   <= "0001" when (addr_i = raddr_data_byte0) else
-                      "0010" when (addr_i = raddr_data_byte1) else
-                      "0100" when (addr_i = raddr_data_byte2) else
-                      "1000";
+    counter_r_we   <= "0001" when (addr_i = waddr_data_byte0) else
+                      "0010" when (addr_i = waddr_data_byte1) else
+                      "0100" when (addr_i = waddr_data_byte2) else
+                      "1000" when (addr_i = waddr_data_byte3) else
+                      "0000";
     
     counter_r_next <= (wdata_i( 8-1 downto 0) &
                        wdata_i( 8-1 downto 0) &
@@ -187,7 +182,8 @@ begin  -- architecture rtl
   gen_counter_write_16b: if SIZE_DATA = 16
   generate
     counter_r_we   <= "0011" when (addr_i = raddr_data_byte0) else
-                      "1100";
+                      "1100" when (addr_i = raddr_data_byte2) else
+                      "0000";
     
     counter_r_next <= (wdata_i(16-1 downto 0) &
                        wdata_i(16-1 downto 0));
@@ -198,7 +194,18 @@ begin  -- architecture rtl
     counter_r_we   <= "1111";
     counter_r_next <= wdata_i(32-1 downto 0);
   end generate gen_counter_write_32b;
+
+  -----------------------------------------------------------------------------
+  -- Counter next value
+  -----------------------------------------------------------------------------
+  counter_cur_r_next <= std_logic_vector(unsigned(counter_cur) - 1);
+
+  -- Event after (counter_r + 2) ticks + 1 cycle
+  counter_cur_r_rst  <= timer_begin;
   
+  counter_cur        <= counter_cur_r;
+
+  counter_test       <= counter_cur_r;
   -----------------------------------------------------------------------------
   -- Register
   -----------------------------------------------------------------------------
@@ -223,7 +230,13 @@ begin  -- architecture rtl
         -- Decrease counter
         if (timer_count = '1')
         then
-          counter_cur_r <= std_logic_vector(unsigned(counter_cur_r) - 1);
+          counter_cur_r <= counter_cur_r_next;
+        end if;
+
+        -- Init counter
+        if (counter_cur_r_rst = '1')
+        then
+          counter_cur_r <= counter_r;
         end if;
 
         -- Disable timer if end is reach
@@ -232,28 +245,18 @@ begin  -- architecture rtl
           timer_enable_r <= '0';
         end if;
 
-        -- Init counter
-        if (timer_begin = '1')
-        then
-          counter_cur_r <= counter_r;
-        end if;
-
         -- User Write
         if (cs_i = '1' and we_i = '1')
         then
           if (addr_i = waddr_control)
           then
             timer_enable_r    <= wdata_i(0);
-            timer_autostart_r <= wdata_i(1);
-            timer_it_enable_r <= wdata_i(2);
-            timer_use_tick_r  <= wdata_i(3);
+            timer_event_r     <= wdata_i(1) and timer_event_r;
+            timer_autostart_r <= wdata_i(2);
+            timer_it_enable_r <= wdata_i(3);
+            timer_use_tick_r  <= wdata_i(4);
           end if;
           
-          if (addr_i = waddr_event)
-          then
-            timer_event_r     <= timer_event_r and wdata_i(0);
-          end if;
-
           for i in 3 downto 0
           loop
             if counter_r_we(i) = '1'
@@ -274,15 +277,39 @@ begin  -- architecture rtl
   end process gen_reg;
 
   -----------------------------------------------------------------------------
+  -- Tick control
+  -----------------------------------------------------------------------------
+  tick_cke   <= cke_i and timer_enable_r and timer_use_tick_r;
+  tick_rst   <= tick_event or timer_begin;
+  tick_event <= '1' when tick_counter_r = 0 else
+                '0';
+  
+  process(clk_i)
+  begin 
+    if rising_edge(clk_i)
+    then
+      if (tick_cke = '1')
+      then
+        if (tick_rst='1')
+        then
+          tick_counter_r <= TICK-1;
+        else
+          tick_counter_r <= tick_counter_r-1;
+        end if;
+      end if;
+    end if;
+  end process;
+  -----------------------------------------------------------------------------
   -- Timer control
   -----------------------------------------------------------------------------
 
-  counter_r_null <= '1' when counter_r = CST0(counter_r'range) else
-                    '0';
+  counter_cur_null   <= '1' when counter_test = CST0(counter_r'range) else
+                        '0';
   
   -- First cycle
   -- or last cycle and autostart
-  timer_begin    <= (timer_enable_r and not timer_enable_r2) or (timer_end and timer_autostart_r);
-  timer_end      <= timer_enable_r and counter_r_null;
-  timer_count    <= timer_enable_r; -- count each cycle
+  timer_begin        <= (timer_enable_r and not timer_enable_r2) or (timer_end and timer_autostart_r);
+  timer_end          <= timer_enable_r and counter_cur_null;
+  timer_count        <= timer_enable_r when timer_use_tick_r = '0' else
+                        tick_event; -- count each cycle
 end architecture rtl;
